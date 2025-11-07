b467fc58010657786408657ff977a29d2afb47d7
Bruno Malizia <bruno.malizia@alkemy.com>
2025-11-07 17:54:58 +0100

    Aggiunta immagine e nuova route

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Ciao Bruno! 🎉 Questa è la tua prima API in Flask dentro Docker."
+    return """
+    <h1>Ciao Bruno! 🎉</h1>
+    <p>Questa è la tua API con un'immagine.</p>
+    <img src="/immagine" width="300">
+    """
+
+@app.route("/immagine")
+def immagine():
+    return send_from_directory("static", "bruno.jpg")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
